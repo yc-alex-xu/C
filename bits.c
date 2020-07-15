cf5c3bbe86a04c21ca63d00823167e309d22b60a
Alex Xu <xuyc@sina.com>
2020-07-15 21:22:58 +0800

    modified:   bits.c

--- bits.c
+++ bits.c
@@ -2,7 +2,8 @@
 #include <string.h>
 #include "toolkit.h"
 
-int count_1_in_bits(unsigned long bitset)
+//计算一个bitset 中有多少个bit 1
+int countOf1(unsigned long bitset)
 {
     int count = 0;
     while (bitset)
@@ -17,28 +18,14 @@ void count_test()
 {
     FUNC_HEAD();
     unsigned long l = 0;
-
-    printf("count of 1 in 0x%lx = %d\n", l, count_1_in_bits(l));
-
-    l += 1 << 0;
-    printf("count of 1 in 0x%lx = %d\n", l, count_1_in_bits(l));
-
-    l += 1 << 1;
-    printf("count of 1 in 0x%lx = %d\n", l, count_1_in_bits(l));
-
-    l += 1 << 2;
-    printf("count of 1 in 0x%lx = %d\n", l, count_1_in_bits(l));
-
-    l += 1 << 4;
-    printf("count of 1 in 0x%lx = %d\n", l, count_1_in_bits(l));
-
-    l += 1 << 9;
-    printf("count of 1 in 0x%lx = %d\n", l, count_1_in_bits(l));
-
-    l += 1 << 15;
-    printf("count of 1 in 0x%lX = %d\n", l, count_1_in_bits(l));
+    for (int i = 0; i < 8; i++)
+    {
+        l += 1 << i;
+        printf("0x%lx has %d of bits 1\n", l, countOf1(l));
+    }
 }
 
+//对整数取反
 int reversal(int a)
 {
     return ~a + 1;
@@ -49,11 +36,11 @@ void reversal_test()
     FUNC_HEAD();
     int i;
     i = 100;
-    printf("original %d, after reversal %d \n", i, reversal(i));
-
+    printf("reversal of  %d = %d \n", i, reversal(i));
     i = -88;
-    printf("original %d, after reversal %d \n", i, reversal(i));
+    printf("reversal of  %d = %d \n", i, reversal(i));
 }
+
 int main()
 {
     count_test();
