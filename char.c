26010e0f584b403e4affe6146649ec5bfd2607cc
Alex Xu <xuyc@sina.com>
2020-07-15 21:37:14 +0800

    modified:   char.c

--- char.c
+++ char.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
+#include "toolkit.h"
+
+/* compiler 缺省优化，会考虑变量空间的paragrah, 所以即使代码中 char a,b 被 int c 隔开
+   空间分配时还是会优化的
+*/
+void char_space_test()
+{
+    FUNC_HEAD();
+    char a;
+    int c;
+    char b;
+    printf("&a=%p\n&b=%p\n&c=%p\n", &a, &b, &c);
+}
+
+void char_sign_test()
+{
+    FUNC_HEAD();
+    char c = -1;
+    /*char 编译器可以实现为带符号的，也可以实现为不带符号的 */
+    printf("char type is:%s\n", c < 0 ? "signed" : "unsigned");
+    char *p;                  //char *:Null-character (‘\0’) terminated character arrays
+    unsigned char *q = "abc"; /* in modern c it  work same as char */
+    printf("%s\n", q);
+
+    for (char i = 126; i < 127; ++i) //if <128, it will loop endless
+    {
+        printf("%d\n", i);
+    }
+}
 
 int main()
 {
-    char c = -1;
-    /*char 编译器可以实现为带符号的，也可以实现为不带符号的 */
-    if (c < 200)
-    {
-        printf("defaut char type is:signed\n");
-    }
-    else
-    {
-        printf("defaut char type is:unsigned\n");
-    }
-
-    for (unsigned char i = 254; i < 255; ++i) //if 256, loop become endless
-    {
-        printf("%d\n", i);
-    }
-    for (char i = 126; i < 127; ++i) //if 128, loop become endless
-    {
-        printf("%d\n", i);
-    }
-
-    char *p; //char *:Null-character (‘\0’) terminated character arrays
-    unsigned char *q = "abc"; /* in modern c it  work same as char */
-    printf("%s\n", q);
-    return 0;
+    char_space_test();
+    char_sign_test();
 }
