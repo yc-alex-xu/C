08cd50c04bf002a3c6e1597ac3f224416fbb7807
Alex Xu <xuyc@sina.com>
2020-08-06 22:18:52 +0800

    modified:   thread/thread_specific_data.c

--- thread/thread_specific_data.c
+++ thread/thread_specific_data.c
@@ -1,48 +1,34 @@
 #include <malloc.h>
 #include <pthread.h>
 #include <stdio.h>
-/* The key used to associate a log file pointer with each thread. */
+
 static pthread_key_t thread_log_key;
 
-/* Write MESSAGE to the log file for the current thread. */
-void write_to_thread_log(const char *message)
+void write_to_thread_log()
 {
-    FILE *thread_log = (FILE *)pthread_getspecific(thread_log_key);
-    fprintf(thread_log, "%s\n", message);
+    char * filename = (char *)pthread_getspecific(thread_log_key);
+    printf("%s \n", filename);
 }
-/* Close the log file pointer THREAD_LOG. */
-void close_thread_log(void *thread_log)
 
-{
-    fclose((FILE *)thread_log);
-}
 void *thread_function(void *args)
 {
-    char thread_log_filename[30];
+    char filename[30];
     FILE *thread_log;
     /* Generate the filename for this thread’s log file. */
-    sprintf(thread_log_filename, "/tmp/thread_%d.log", (int)pthread_self());
-    /* Open the log file. */
-    thread_log = fopen(thread_log_filename, "w");
+    sprintf(filename, "thread_%d.log", (int)pthread_self());
     /* Store the file pointer in thread-specific data under thread_log_key. */
-    pthread_setspecific(thread_log_key, thread_log);
-    write_to_thread_log("Thread starting.");
+    pthread_setspecific(thread_log_key, filename);
+    write_to_thread_log();
     /* Do work here... */
     return NULL;
 }
 
 void *thread_function2(void *args) //added by Alex
 {
-    static __thread char thread_log_filename[30];
-    FILE *thread_log;
+    static __thread char filename[30];
     /* Generate the filename for this thread’s log file. */
-    sprintf(thread_log_filename, "/tmp/thread_%d.log", (int)pthread_self());
-    /* Open the log file. */
-    thread_log = fopen(thread_log_filename, "w");
-    /* Store the file pointer in thread-specific data under thread_log_key. */
-    pthread_setspecific(thread_log_key, thread_log);
-    write_to_thread_log("Thread starting.");
-    /* Do work here... */
+    sprintf(filename, "thread_%d.log", (int)pthread_self());
+    printf("%s\n",filename);
     return NULL;
 }
 int main()
@@ -50,20 +36,20 @@ int main()
     int i;
     pthread_t threads[5]; //thread id will be reused if thread terminated
 
-    pthread_key_create(&thread_log_key, close_thread_log);
+    pthread_key_create(&thread_log_key, NULL);
 
     for (i = 0; i < 2; ++i)
     {
         pthread_create(&(threads[i]), NULL, thread_function, NULL);
-        printf("thread %d is created !\n", (int)threads[i]);
+        printf("thread %lu is created !\n", threads[i]);
     }
     //another way of TSD
-    for (i = 2; i < 5; ++i)
+    for (i = 2; i < 4; ++i)
     {
         pthread_create(&(threads[i]), NULL, thread_function2, NULL);
-        printf("thread %d is created !\n", (int)threads[i]);
+        printf("thread %lu is created !\n", threads[i]);
     }
-    for (i = 0; i < 5; ++i)
+    for (i = 0; i < 4; ++i)
         pthread_join(threads[i], NULL);
 
     return 0;
