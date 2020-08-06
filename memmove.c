4e791245fb96cac611c260cfc07775e7fe61f5d1
Alex Xu <xuyc@sina.com>
2020-08-06 15:03:42 +0800

    modified:   memmove.c

--- memmove.c
+++ memmove.c
@@ -1,7 +1,6 @@
- #include <stdio.h>
- #include <string.h>
-/*
+#include <stdio.h>
 
+/*
 __np_anyptrlt(p1,p2)：A macro or function such that, for any two pointers p1 and p2，
 __np_anyptrlt(p1,p2) evaluates to:
 non-zero if p1 and p2 point within the same object and p1 is less than p2；
@@ -21,16 +20,28 @@ void *memmove(void *dest, const void *src, size_t n)
             *pd++ = *ps++;
     return dest
 }
-
-
-
 */
+
+void *memmove(void *dest, const void *src, size_t n)
+{
+    unsigned char *pd = dest;
+    const unsigned char *ps = src;
+    if (ps < pd)
+        for (pd += n, ps += n; n--;) //小技巧
+            *--pd = *--ps;
+    else
+        while (n--)
+            *pd++ = *ps++;
+    return dest;
+}
+
 int main()
 {
-    char a[10]="abc";
-    memmove(&a[1],a,strlen(a));
-    printf ("%s\n",a);
-
+    char str[10] = "abcdef";
+    memmove(&str[1], str, 6);
+    printf("%s\n", str);
+    //"bcdef",sz=6的话把最后的'\0'也move过去了
+    memmove(&str[1], &str[2], 6);
+    printf("%s\n", str);
     return 0;
-
 }
